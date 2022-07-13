d50b40a78851ad465d11a9b6d84467089c486a50
Mehdi Desoeuvre <mehdi.desoeuvre@gmail.com>
2022-07-13 15:15:38 +0200

    postgres + pgadmin docker-compose; typeorm install

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,23 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { SocketGateway } from './socket.gateway';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'root',
+      password: 'myrootpassword',
+      database: 'user_db',
+      entities: ["dist/**/*.entity{.ts,.js}"],
+      // synchronize: true => dev only
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService, SocketGateway],
 })
