3d7b302c6c5ae3988667f269789d05ce5537170d
Mehdi Desoeuvre <mehdi.desoeuvre@gmail.com>
2022-08-01 15:55:41 +0200

    file upload renamed as user.extension

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,12 +1,14 @@
 import { Body, Controller, Delete, Get, Param, ParseIntPipe, Post, UploadedFile, UploadedFiles, UseGuards, UseInterceptors } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
-import { FilesInterceptor } from '@nestjs/platform-express';
+import { FileInterceptor, FilesInterceptor } from '@nestjs/platform-express';
 import { MResponse } from 'src/MResponse';
 import { AddUserDto, SendUserDto } from './dto/user.dto';
 import { UserEntity } from './entities/user.entity';
 import { UsersService } from './users.service';
 import { Express } from 'express';
-import { Multer } from 'multer';
+import { diskStorage, Multer } from 'multer';
+import { request } from 'http';
+import path from 'path';
 
 @Controller('users')
 export class UsersController {
@@ -40,9 +42,20 @@ export class UsersController {
 			return this.usersService.softRemoveUser(id);
 		}
 		
-	@Post('upload')
-	@UseInterceptors(FilesInterceptor('image'))
-	uploadFile(@UploadedFile() file: Express.Multer.File) {
-	console.log(file);
+	@Post('upload/:user')
+	@UseInterceptors(FileInterceptor('file', {
+		storage: diskStorage({
+			destination: './uploads',
+			filename:  function (req, file, cb) {
+				cb(null, req.params.user + '.' + file.mimetype.split('/')[1]);
+			  }
+			})
+		}))
+	uploadFile(
+		@UploadedFile() file: Express.Multer.File,
+		@Param() user: string
+		): void {
+	console.log(user, file);
+	console.log(file.mimetype);
 	}
 }
