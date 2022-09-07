d159d2fb2dd032bc62fa29f51b091ddde69004e9
Maximilien Rozniecki <blueyesmax69@gmail.com>
2022-09-07 12:23:16 +0200

    start Pong serverSide first commit

--- src/game/entities/game.entity.ts
+++ src/game/entities/game.entity.ts
@@ -1,6 +1,23 @@
 import {UserEntity} from "src/users/entities/user.entity";
 import {Column, Entity, ManyToOne, OneToOne, PrimaryGeneratedColumn} from "typeorm";
 
+
+export type Game = {
+    p1Score: number;
+    p2Score: number;
+    ballX: number;
+    ballY: number;
+    ballSpeed: number;
+    ballVelocityX: number;
+    ballVelocityY: number;
+    padLeftX: number;
+    padLeftY: number;
+    padLeftVelocity: number;
+    padRightX: number;
+    padRightY: number;
+    padRightVelocity: number;
+}
+
 export enum GameStatus {
     PENDING = 0,
     RUNNING = 1,
--- src/game/game.service.ts
+++ src/game/game.service.ts
@@ -2,6 +2,7 @@ import {Injectable} from '@nestjs/common';
 import {InjectRepository} from '@nestjs/typeorm';
 import {Repository} from 'typeorm';
 import {GameEntity} from './entities/game.entity';
+import {Game} from './entities/game.entity';
 
 @Injectable()
 export class GameService {
@@ -9,6 +10,29 @@ export class GameService {
         @InjectRepository(GameEntity)
         private gameRepository: Repository<GameEntity>,
     ) {
+    }
+    //800 / 600
+    gameInit(game: Game) {
+        game.p1Score = 0;
+        game.p2Score = 0;
+        this.setInit(game);
+    }
+
+    setInit(game: Game) {
+        game.ballX = 50;
+        game.ballY = 6.67;
+        game.ballSpeed = 5;
+        if (game.p2Score > game.p1Score)
+            game.ballVelocityX = game.ballSpeed * Math.cos(Math.PI / 4);
+        else
+            game.ballVelocityX = -game.ballSpeed * Math.cos(Math.PI / 4);
+        game.ballVelocityY = game.ballSpeed*Math.sin(Math.PI / 4);
+        game.padLeftX = 2.5;
+        game.padLeftY = 50;
+        game.padLeftVelocity = 0;
+        game.padLeftX = 97.5;
+        game.padLeftY = 50;
+        game.padLeftVelocity = 0;
     }
 
 
