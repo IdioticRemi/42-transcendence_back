e970ff7b16e78653965d755eeef4dd760e9a5e8c
mdesoeuv <mehdi.desoeuvre@gmail.com>
2022-09-20 13:40:50 +0200

    42 ip address + uploads dir creation

--- src/main.ts
+++ src/main.ts
@@ -5,11 +5,11 @@ import * as fs from 'fs';
 
 async function bootstrap() {
 
-    const httpsOptions = {
-        key: fs.readFileSync('./cert/key.pem'),
-        cert: fs.readFileSync('./cert/cert.pem'),
-    }
-    const app = await NestFactory.create(AppModule, {httpsOptions});
+    // const httpsOptions = {
+    //     key: fs.readFileSync('./cert/key.pem'),
+    //     cert: fs.readFileSync('./cert/cert.pem'),
+    // }
+    const app = await NestFactory.create(AppModule, /*{httpsOptions}*/);
     app.enableCors(({
         "origin": "*",
         "methods": "GET,PUT,POST,DELETE",
